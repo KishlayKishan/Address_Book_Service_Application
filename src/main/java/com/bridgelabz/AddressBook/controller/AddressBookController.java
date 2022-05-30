02f68150a08c329d9ad8b319a34b662fcc10e467
Kishlay Kishan <kishan.kishlay20@gmail.com>
2022-05-30 12:39:43 +0530

    Added : Provide user Friendly Error

--- src/main/java/com/bridgelabz/AddressBook/controller/AddressBookController.java
+++ src/main/java/com/bridgelabz/AddressBook/controller/AddressBookController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/addressbook")
-public class AddressbookController {
+public class AddressBookController {
 
     @Autowired
     IAddressBookService addressbookService;
